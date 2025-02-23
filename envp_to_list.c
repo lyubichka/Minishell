346511ad8364161b1c5f297dd208d31ad548ddd6
saherrer-git <saherrer@student.42abudhabi.ae>
2025-02-23 22:24:22 +0400

    minor change on envp_to_list

--- envp_to_list.c
+++ envp_to_list.c
@@ -6,7 +6,7 @@
 /*   By: saherrer <saherrer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 20:31:40 by saherrer          #+#    #+#             */
-/*   Updated: 2025/02/23 22:12:36 by saherrer         ###   ########.fr       */
+/*   Updated: 2025/02/23 22:23:49 by saherrer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int envp_to_list(char **envp, char **env_list)
 	char *env_name;
 	char *env_value;
 	int eq_pos;
+	t_env *node;
 
 	i = 0;	
 	while(envp[i])
@@ -27,7 +28,8 @@ int envp_to_list(char **envp, char **env_list)
 			eq_pos = ft_strchr(envp[i], '=') - envp[i];
 			env_name = ft_substr(envp[i], 0, eq_pos);
 			env_value = ft_substr(envp[i], eq_pos + 1, ft_strlen(envp[i]) - eq_pos - 1);
-			lst_add_back(lst_create_envp(env_name, env_value), env_list);
+			node = lst_create_envp(env_name, env_value);
+			lst_add_back(node, env_list);
 		}
 		i++;
 	}
