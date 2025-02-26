aa7ab237c56802eebb09964dcd07924d020be774
saherrer-git <saherrer@student.42abudhabi.ae>
2025-02-26 21:49:42 +0400

    only removed commments

--- srcs/check_quotes.c
+++ srcs/check_quotes.c
@@ -6,62 +6,11 @@
 /*   By: saherrer <saherrer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 20:49:57 by saherrer          #+#    #+#             */
-/*   Updated: 2025/02/26 21:46:13 by saherrer         ###   ########.fr       */
+/*   Updated: 2025/02/26 21:49:27 by saherrer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-// static int	check_single(char *line)
-// {
-// 	int	i;
-// 	int	count;
-	
-// 	i = 0;
-// 	count = 0;
-// 	while (line[i] != '\0')
-// 	{
-// 		if(line[i] == '\'')
-// 			count++;
-// 		i++;
-// 	}
-// 	if(count % 2 == 0)
-// 		return (0);
-// 	else
-// 	{
-// 		ft_putstr_fd("Error: found not closed single quote\n", 2);
-// 		return (1);
-// 	}
-// }
-
-// static int	check_double(char *line)
-// {
-// 	int	i;
-// 	int	count;
-	
-// 	i = 0;
-// 	count = 0;
-// 	while (line[i] != '\0')
-// 	{
-// 		if(line[i] == '\"')
-// 			count++;
-// 		i++;
-// 	}
-// 	if(count % 2 == 0)
-// 		return (0);
-// 	else
-// 	{
-// 		ft_putstr_fd("Error: found not closed double quote\n", 2);
-// 		return (1);
-// 	}
-// }
-
-// int	check_quotes(char *line)
-// {
-// 	if (check_single(line) == 1 || check_double(line) == 1)
-// 		return (1);
-// 	return (0);
-// }
 
 int	check_quotes(char *line)
 {
