94552acba5094ebb26371fab4b050b4d0c32e9a4
saherrer-git <saherrer@student.42abudhabi.ae>
2025-03-27 20:13:03 +0400

    fixed tokenizer to account for consecutive quotes

--- srcs/tokenizer.c
+++ srcs/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: saherrer <saherrer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 19:58:17 by saherrer          #+#    #+#             */
-/*   Updated: 2025/03/26 21:08:55 by saherrer         ###   ########.fr       */
+/*   Updated: 2025/03/27 19:29:40 by saherrer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,20 +25,29 @@ static void	extract_quote(t_token **tokens, char *line, int *line_pos)
 	while (line[j] != '\0')
 	{
 		if (line[j] == '\\' && (line[j + 1] == '\'' || line[j + 1] == '\"'))
-			j++;  // Skip the escape character
+			j++;  // Skip escaped quote
 		else if (line[j] == quote)  // Found closing quote
-			break ;
+		{
+			j++;  // Move past closing quote
+			if (is_quote(line[j])) // Check if a new quote starts immediately
+			{
+				quote = line[j]; // Set new quote type
+				continue;  // Restart the loop without breaking
+			}
+			else
+				break;  // Stop scanning if no more consecutive quotes
+		}
 		j++;
 	}
 	extract = ft_substr(line, *line_pos, j - *line_pos + 1);
-	if (quote == '\'')
-		new_token = token_create('w', extract, 1);
-	else
-		new_token = token_create('w', extract, 2);
+	// if (quote == '\'')
+	// 	new_token = token_create('w', extract, 1);
+	// else
+		new_token = token_create('w', extract, 2); // need to review this "is quote" in the file of the token
 	lst_token_add_back(new_token, tokens);
-	if (line[j] != '\0')
-		*line_pos = j + 1;
-	else
+	// if (line[j] != '\0')
+	// 	*line_pos = j + 1;
+	// else
 		*line_pos = j;
 }
 
@@ -65,29 +74,66 @@ static void	extract_operator(t_token **tokens, char *line, int *line_pos, char *
 		*line_pos = j;
 }
 
+// static void	extract_word(t_token *tokens, char *line, int *line_pos, char *delimiters)
+// {
+// 	int		j;
+// 	char	*extract;
+// 	t_token	*new_token;
+
+// 	j = *line_pos;
+// 	while (line[j] != '\0')
+// 	{
+// 		if (is_quote(line[j + 1]) && line[j] != '\\')
+// 			break ;
+// 		else if (is_operator(line[j + 1], delimiters) && line[j] != '\\')
+// 			break ;
+// 		else
+// 			j++;
+// 	}
+// 	extract = ft_substr(line, *line_pos, j - *line_pos + 1);
+// 	new_token = token_create('w', extract, 0);
+// 	lst_token_add_back(new_token, tokens);
+// 	if (line[j] != '\0')
+// 		*line_pos = j + 1;
+// 	else
+// 		*line_pos = j;
+// }
+
 static void	extract_word(t_token *tokens, char *line, int *line_pos, char *delimiters)
 {
 	int		j;
+	char	quote;
 	char	*extract;
 	t_token	*new_token;
 
 	j = *line_pos;
-	while (line[j] != '\0')
+	while (line[j] != '\0' && !is_operator(line[j], delimiters))  // Stop only on operators
 	{
-		if (is_quote(line[j + 1]) && line[j] != '\\')
-			break ;
-		else if (is_operator(line[j + 1], delimiters) && line[j] != '\\')
-			break ;
-		else
-			j++;
+		if (is_quote(line[j]))  // Found opening quote
+		{
+			quote = line[j];
+			j++;  // Move past opening quote
+			while (line[j] != '\0')
+			{
+				if (line[j] == '\\' && (line[j + 1] == quote))
+					j++;  // Skip escaped quote
+				else if (line[j] == quote)  // Found closing quote
+				{
+					j++;  // Move past closing quote
+					break;
+				}
+				j++;
+			}
+			continue;  // Continue scanning after handling the quote
+		}
+		j++;
 	}
-	extract = ft_substr(line, *line_pos, j - *line_pos + 1);
+	// Extract the full word, including quoted sections
+	extract = ft_substr(line, *line_pos, j - *line_pos);
 	new_token = token_create('w', extract, 0);
 	lst_token_add_back(new_token, tokens);
-	if (line[j] != '\0')
-		*line_pos = j + 1;
-	else
-		*line_pos = j;
+	// Move position forward
+	*line_pos = j;
 }
 
 static int	check_operators(t_token *tokens)
@@ -113,6 +159,32 @@ static int	check_operators(t_token *tokens)
 			}
 		}
 		tokens = tokens->next;
+	}
+	return (1);
+}
+
+int	tokenizer(t_token **tokens, char *line, char *delimiters, t_env **env_list)
+{
+	int	i;
+	int	end_token;
+
+	i = 0;
+	while (line[i] != '\0')
+	{
+		if (is_quote(line[i]) == 1)
+			extract_quote(tokens, line, &i);
+		else if (is_operator(line[i], delimiters) == 1)
+			extract_operator(tokens, line, &i, delimiters);
+		else
+			extract_word(tokens, line, &i, delimiters);
+	}
+	token_split(tokens);
+	token_cleanup(tokens);
+	if (check_operators(*tokens) == -1 || first_or_last_is_pipe(*tokens) == -1)
+	{
+		update_exit_status(1, env_list);
+		lst_clear_tokens(tokens);
+		return (-1);
 	}
 	return (1);
 }
