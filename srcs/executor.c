2fd075ae683f8900dddfadd23950d63ef3ab8171
Veronika Liubitckaia <100288923+lyubichka@users.noreply.github.com>
2025-04-14 17:52:26 +0400

    Update executor.c
    get_env_value, search_in_path, find_command_path

--- srcs/executor.c
+++ srcs/executor.c
@@ -12,29 +12,37 @@
 
 #include "minishell.h"
 
-static void setup_input(t_command *cmd)
+void	exit_static_status(t_env *env, int status) // to add
+{
+    if (env)
+        env->exit_status = status;
+}
+
+static void	setup_input(t_command *cmd)
 {
     if (cmd->fd_in >= 0)
     {
-        if (dup2(cmd->fd_in, STDIN_FILENO) == -1)
-        {
-            ft_putstr_fd("dup2 fd_in", 2);
-            exit(1);
-        }
+        dup2(cmd->fd_in, STDIN_FILENO);
+        close(cmd->fd_in);
+    }
+    else if (cmd->pipe_in)
+    {
+        dup2(cmd->fd_in, STDIN_FILENO);
         close(cmd->fd_in);
     }
 }
 
 
-static void setup_output(t_command *cmd)
+static void	setup_output(t_command *cmd)
 {
     if (cmd->fd_out >= 0)
     {
-        if (dup2(cmd->fd_out, STDOUT_FILENO) == -1)
-        {
-            ft_putstr_fd("dup2 fd_out", 2);
-            exit(1);
-        }
+        dup2(cmd->fd_out, STDOUT_FILENO);
+        close(cmd->fd_out);
+    }
+    else if (cmd->pipe_out)
+    {
+        dup2(cmd->fd_out, STDOUT_FILENO);
         close(cmd->fd_out);
     }
 }
@@ -90,9 +98,9 @@ static void run_external_command(t_command *cmd, t_env **env_list)
 {
     setup_input(cmd);
     setup_output(cmd);
-    execve(cmd->path, cmd->argv, env_list_to_array(*env_list)); // Executes an external program, replacing the current process
-    ft_putstr_fd("minishell: execve failed\n", 2);
-    exit_static_status(127);
+    execve(cmd->path, cmd->argv, env_list_to_array(*env_list)); //  change to NULL ? so that the new program inherits the environment of the current process
+    ft_putstr_fd("minishell: command not found\n", 2);
+    exit_static_status(*env_list, 127); // to do
     exit(127);
 }
 
@@ -130,28 +138,95 @@ static void run_builtin(t_command *cmd, t_env **env_list)
         ft_putstr_fd("minishell: unknown builtin\n", 2);
 }
 
-void execute_command(t_command *cmd, t_env **env_list)
+static char *get_env_value(t_env *env_list, char *name)
 {
-    pid_t    pid;
-    
+    while (env_list)
+    {
+        if (ft_strncmp(env_list->name, name) == 0)
+            return (env_list->value);
+        env_list = env_list->next;
+    }
+    return (NULL);
+}
+
+static char *search_in_path(t_env *env_list, char *cmd)
+{
+    char **paths;
+    char *path;
+    char *tmp;
+    int i;
+
+    tmp = get_env_value(env_list, "PATH");
+    if (!tmp)
+        return (NULL);
+    paths = ft_split(tmp, ':');
+    i = 0;
+    while (paths[i])
+    {
+        path = ft_strjoin(paths[i], "/");
+        tmp = path;
+        path = ft_strjoin(tmp, cmd);
+        free(tmp);
+        if (access(path, X_OK) == 0)
+        {
+            free_array(paths);
+            return (path);
+        }
+        free(path);
+        i++;
+    }
+    free_array(paths);
+    return (NULL);
+}
+
+static char *find_command_path(t_command *cmd, t_env *env_list)
+{
+    if (!cmd->argv || !cmd->argv[0])
+        return (NULL);
+    if (cmd->is_builtin || strchr(cmd->argv[0], '/'))
+        return (ft_strdup(cmd->argv[0]));
+    return (search_in_path(env_list, cmd->argv[0]));
+}
+
+void	execute_command(t_command *cmd, t_env **env_list)
+{
+    pid_t pid;
+    int status;
+
     while (cmd)
     {
-        if (cmd->is_builtin)
+        if (!cmd->argv || !cmd->argv[0] || cmd->argv[0][0] == '\0')
         {
+            ft_putstr_fd("minishell: '': command not found\n", 2);
+            exit_static_status(*env_list, 127); // to do
+            cmd = cmd->next;
+            continue;
+        }
+        cmd->path = find_command_path(cmd, *env_list);
+        if (!cmd->path && !cmd->is_builtin)
+        {
+            ft_putstr_fd("minishell: command not found\n", 2);
+            exit_static_status(*env_list, 127); // To do
+            cmd = cmd->next;
+            continue;
+        }
+        if (cmd->is_builtin && !cmd->is_pipe)
             run_builtin(cmd, env_list);
-            return;
-        }
-        pid = fork(); // creating a child process
-        if (pid == -1)
-        {
-            ft_putstr_fd("minishell: fork error\n", 2);
-            exit_static_status(1);
-            return;
-        }
-        if (pid == 0) 
-            run_external_command(cmd, env_list);
         else
-            handle_parent_process(cmd, pid);
+        {
+            pid = fork();
+            if (pid == 0)
+            {
+                if (cmd->is_builtin)
+                    run_builtin(cmd, env_list);
+                else
+                    run_external(cmd, env_list);
+                exit(0);
+            }
+        }
         cmd = cmd->next;
     }
+    while (wait(&status) > 0)
+        if (WIFEXITED(status))
+            exit_static_status(*env_list, WEXITSTATUS(status));
 }
