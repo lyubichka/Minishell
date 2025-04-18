787b0253e7b4b2f8b484b992aa443628d087f4ba
Veronika Liubitckaia <100288923+lyubichka@users.noreply.github.com>
2025-04-18 19:26:11 +0400

    Update line_var_expansion.c norm

--- srcs/line_var_expansion.c
+++ srcs/line_var_expansion.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   line_var_expansion.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: saherrer <saherrer@student.42.fr>          +#+  +:+       +#+        */
+/*   By: veronikalubickaa <veronikalubickaa@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 18:20:04 by saherrer          #+#    #+#             */
-/*   Updated: 2025/04/17 22:05:16 by saherrer         ###   ########.fr       */
+/*   Updated: 2025/04/18 19:25:46 by veronikalub      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ static void	var_not_found_line(char **line, char *var_name, int *pos_value)
 	suffix_start = *pos_value + ft_strlen(var_name) + 1;
 	total_len = prefix_len + ft_strlen(*line + suffix_start);
 	new_line_value = (char *)malloc(sizeof(char) * (total_len + 1));
-	if(!new_line_value)
+	if (!new_line_value)
 		return ;
 	ft_strlcpy(new_line_value, *line, prefix_len + 1);
 	ft_strlcat(new_line_value, *line + suffix_start, total_len + 1);
@@ -31,7 +31,8 @@ static void	var_not_found_line(char **line, char *var_name, int *pos_value)
 	*line = new_line_value;
 }
 
-static void	replace_var_line(char **line, char *var_value, char *var_name, int *pos_value)
+static void	replace_var_line(char **line, char *var_value, char *var_name,
+		int *pos_value)
 {
 	char	*new_line_value;
 	int		prefix_len;
@@ -45,42 +46,42 @@ static void	replace_var_line(char **line, char *var_value, char *var_name, int *
 	total_len = prefix_len + var_len + ft_strlen(*line + suffix_start);
 	new_line_value = (char *)malloc(sizeof(char) * (total_len + 1));
 	if (!new_line_value)
-		return;
+		return ;
 	ft_strlcpy(new_line_value, *line, prefix_len + 1);
 	ft_strlcat(new_line_value, var_value, total_len + 1);
 	ft_strlcat(new_line_value, *line + suffix_start, total_len + 1);
 	free(*line);
 	*line = new_line_value;
 	*pos_value = prefix_len + var_len;
-	
 }
 
 static void	found_exit_var_line(char **line, int *pos_value)
 {
-	int j;
-	char *var_name;
-	char *exit_code_str;
+	int		j;
+	char	*var_name;
+	char	*exit_code_str;
 
 	j = *pos_value + 2;
 	var_name = ft_substr(*line, *pos_value + 1, j - *pos_value - 1);
 	exit_code_str = ft_itoa(exit_static_status(-1));
 	replace_var_line(line, exit_code_str, var_name, pos_value);
-	free(exit_code_str);		
+	free(exit_code_str);
 	free(var_name);
 }
 
 static void	find_and_expand_line(char **line, t_env *env_list, int *pos_value)
 {
-	int j;
-	char *var_name;
-	
+	int		j;
+	char	*var_name;
+
 	j = *pos_value + 1;
 	while ((*line)[j] && (ft_isalnum((*line)[j]) || (*line)[j] == '_'))
 		j++;
 	var_name = ft_substr(*line, *pos_value + 1, j - *pos_value - 1);
-	while (env_list && ft_strncmp(env_list->name, var_name, ft_strlen(var_name) + 1) != 0)
+	while (env_list && ft_strncmp(env_list->name, var_name, ft_strlen(var_name)
+			+ 1) != 0)
 		env_list = env_list->next;
-	if (env_list)	
+	if (env_list)
 		replace_var_line(line, env_list->value, var_name, pos_value);
 	else
 		var_not_found_line(line, var_name, pos_value);
@@ -89,7 +90,7 @@ static void	find_and_expand_line(char **line, t_env *env_list, int *pos_value)
 
 void	line_var_expansion(char **line_to_expand, t_env *env_list)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while ((*line_to_expand)[i] != '\0')
@@ -100,8 +101,8 @@ void	line_var_expansion(char **line_to_expand, t_env *env_list)
 				found_exit_var_line(line_to_expand, &i);
 			else
 				find_and_expand_line(line_to_expand, env_list, &i);
-			continue ; // Skip the rest of the loop after expansion
+			continue ;
 		}
-		i++;  // Continue scanning the next character
+		i++;
 	}
 }
