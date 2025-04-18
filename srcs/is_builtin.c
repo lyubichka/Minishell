5507b910e0265853c809911952460bacffe629ba
Veronika Liubitckaia <100288923+lyubichka@users.noreply.github.com>
2025-04-18 18:08:13 +0400

    Update is_builtin.c norm

--- srcs/is_builtin.c
+++ srcs/is_builtin.c
@@ -1,28 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.c                                            :+:      :+:    :+:   */
+/*   is_builtin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: saherrer <saherrer@student.42.fr>          +#+  +:+       +#+        */
+/*   By: veronikalubickaa <veronikalubickaa@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 04:31:12 by veronikalub       #+#    #+#             */
-/*   Updated: 2025/04/17 21:58:55 by saherrer         ###   ########.fr       */
+/*   Updated: 2025/04/18 18:07:08 by veronikalub      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int is_builtin(char *cmd)
+int	is_builtin(char *cmd)
 {
 	if (!cmd)
 		return (0);
-	if (ft_strncmp(cmd, "cd", 3) == 0 ||
-		ft_strncmp(cmd, "echo", 5) == 0 ||
-		ft_strncmp(cmd, "env", 4) == 0 ||
-		ft_strncmp(cmd, "pwd", 4) == 0 ||
-		ft_strncmp(cmd, "export", 7) == 0 ||
-		ft_strncmp(cmd, "unset", 6) == 0 ||
-		ft_strncmp(cmd, "exit", 5) == 0)
+	if (ft_strncmp(cmd, "cd", 3) == 0 || ft_strncmp(cmd, "echo", 5) == 0
+		|| ft_strncmp(cmd, "env", 4) == 0 || ft_strncmp(cmd, "pwd", 4) == 0
+		|| ft_strncmp(cmd, "export", 7) == 0 || ft_strncmp(cmd, "unset", 6) == 0
+		|| ft_strncmp(cmd, "exit", 5) == 0)
 		return (1);
 	return (0);
 }
