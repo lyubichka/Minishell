e7cb58823073c44e8e35855e27a531a19cb87846
Veronika Liubitckaia <100288923+lyubichka@users.noreply.github.com>
2025-04-18 19:29:35 +0400

    Update lst_clear.c norm

--- srcs/lst_clear.c
+++ srcs/lst_clear.c
@@ -14,8 +14,8 @@
 
 void	lst_clear_tokens(t_token **tokens)
 {
-	t_token *tmp1;
-	t_token *tmp2;
+	t_token	*tmp1;
+	t_token	*tmp2;
 
 	tmp1 = *tokens;
 	while (tmp1)
@@ -31,8 +31,8 @@ void	lst_clear_tokens(t_token **tokens)
 
 void	lst_clear_env(t_env **env)
 {
-	t_env *tmp1;
-	t_env *tmp2;
+	t_env	*tmp1;
+	t_env	*tmp2;
 
 	tmp1 = *env;
 	while (tmp1)
@@ -50,8 +50,8 @@ void	lst_clear_env(t_env **env)
 
 void	lst_clear_commands(t_command **commands)
 {
-	t_command *tmp1;
-	t_command *tmp2;
+	t_command	*tmp1;
+	t_command	*tmp2;
 
 	tmp1 = *commands;
 	while (tmp1)
